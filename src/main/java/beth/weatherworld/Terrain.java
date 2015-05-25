2766272da0212f92a64090d8ab647c0606eacab0
beth <abethcrane@gmail.com>
2015-05-25 14:32:53 -0700

    Clean up terrain - switch from java.awt.Dimension to just width/depth
    Catch array out of bounds exceptions.
    Update center to be using altitude at that point.
    Rearrange imports to be alphabetical

--- src/main/java/beth/weatherworld/Terrain.java
+++ src/main/java/beth/weatherworld/Terrain.java
@@ -1,13 +1,15 @@
 package beth.weatherworld;
 
-import java.awt.Dimension;
-import java.awt.geom.Point2D;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLProfile;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.lang.Object.*;
 import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.HashMap;
@@ -15,17 +17,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Vector;
-import java.lang.Object.*;
-
-import com.jogamp.opengl.GL2;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
-
-import com.jogamp.opengl.GL2;
-import com.jogamp.opengl.GLAutoDrawable;
-import com.jogamp.opengl.GLProfile;
 
 import beth.weatherworld.Texture;
 
@@ -36,12 +30,13 @@ import beth.weatherworld.Texture;
  */
 public class Terrain {
 
-    private Dimension mySize;
+    private int myWidth;
+    private int myDepth;
     private double[][] myAltitude;
     private List<Tree> myTrees;
     private List<Road> myRoads;
     private Point mySunlight;
-    static Point centre;
+    static Point center;
     Texture myTexture;
     public boolean texture = true;
 
@@ -52,19 +47,20 @@ public class Terrain {
      * @param depth The number of vertices in the z-direction
      */
     public Terrain(int width, int depth) {
-        mySize = new Dimension(width, depth);
+        myWidth = width;
+        myDepth = depth;
         myAltitude = new double[width][depth];
         myTrees = new ArrayList<Tree>();
         myRoads = new ArrayList<Road>();
-        centre = new Point(width/2, 0, depth/2);
+        center = new Point(width/2, 0, depth/2);
     }
     
-    public Terrain(Dimension size) {   
-        this(size.width, size.height);
+    public int width() {
+        return myWidth;
     }
-
-    public Dimension size() {
-        return mySize;
+    
+    public int depth() {
+        return myDepth;
     }
 
     public List<Tree> trees() {
@@ -96,29 +92,25 @@ public class Terrain {
      * Resize the terrain, copying any old altitudes. 
      * 
      * @param width
-     * @param height
+     * @param depth
      */
-    public void setSize(int width, int height) {
-        mySize = new Dimension(width, height);
+    public void setSize(int width, int depth) {
+        myWidth = width;
+        myDepth = depth;
         double[][] oldAlt = myAltitude;
-        myAltitude = new double[width][height];
-        
-        double min = 0;
-        double max = 0;
-        
-        for (int i = 0; i < width && i < oldAlt.length; i++) {
-            for (int j = 0; j < height && j < oldAlt[i].length; j++) {
-                myAltitude[i][j] = oldAlt[i][j];
-                if (myAltitude[i][j] < min) {
-                	min = myAltitude[i][j];
-                }
-                if (myAltitude[i][j] > max) {
-                	max = myAltitude[i][j];
-                }
+        myAltitude = new double[width][depth];
+
+        for (int x = 0; x < width && x < oldAlt.length; x++) {
+            for (int z = 0; z < depth && z < oldAlt[x].length; z++) {
+                myAltitude[x][z] = oldAlt[x][z];
             }
         }
         
-        centre.y = (max+min)/2;
+        setCenter();
+    }
+    
+    public void setCenter() {
+        center = new Point(myWidth/2, altitude(myWidth/2, myDepth/2), myDepth/2);
     }
 
     /**
@@ -157,22 +149,27 @@ public class Terrain {
         //v1 = a -az + bz;
         //v2 = c -cz + dz;
         //v3 = v1 -v1x + v2x
-
-        if (Math.floor(x) < 0 || Math.floor(z) < 0 || Math.ceil(x) >= mySize.height || Math.ceil(z) >= mySize.width) {
+        
+        if (Math.floor(x) < 0 || Math.floor(z) < 0 || Math.ceil(x) >= myWidth || Math.ceil(z) >= myDepth) {
         	return altitude;
         }
+
+        try {
+            int leftX = (int)Math.floor(x);
+            int rightX = (int) Math.ceil(x);
+            int farZ = (int)Math.floor(z);
+            int nearZ = (int)Math.ceil(z);
+            
+            double zFraction = z - (double)farZ;
+            double xFraction = x - (double)leftX;
+            
+            double p1 = myAltitude[leftX][farZ] - (myAltitude[leftX][farZ] * zFraction) + (myAltitude[leftX][nearZ] * zFraction);
+            double p2 = myAltitude[rightX][farZ] - (myAltitude[leftX][farZ] * zFraction) + (myAltitude[rightX][nearZ] * zFraction);
+            altitude = p1 - (p1 * xFraction) + (p2 * xFraction);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array out of bounds - x: " + x + " out of " + myWidth + ", z: " + z + " out of " + myDepth);
+        }
         
-        int topX = (int)Math.floor(x);
-        int bottomX = (int) Math.ceil(x);
-        int leftZ = (int)Math.floor(z);
-        int rightZ = (int)Math.ceil(z);
-        
-        double zFraction = z - (double)leftZ;
-        double xFraction = x - (double)topX;
-        
-        double p1 = myAltitude[topX][leftZ] - (myAltitude[topX][leftZ] * zFraction) + (myAltitude[topX][rightZ] * zFraction);
-        double p2 = myAltitude[bottomX][leftZ] - (myAltitude[bottomX][leftZ] * zFraction) + (myAltitude[bottomX][rightZ] * zFraction);
-        altitude = p1 - (p1 * xFraction) + (p2 * xFraction);
         return altitude;
     }
 
@@ -210,7 +207,6 @@ public class Terrain {
 		gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_FILL);
     	gl.glPolygonOffset(0,0);
 		
-		
 		Point p1 = new Point();
 		Point p2 = new Point();
 		Point p3 = new Point();
@@ -231,63 +227,63 @@ public class Terrain {
 
 		gl.glBegin(GL2.GL_TRIANGLES);
 		
-			for (int x = 0; x < size().width - 1; x++) {
-				for (int z = 0; z < size().height - 1; z++) {
-	
-					//Triangle 1 - top right, top left, bottom left,
-					p1.reset(x, myAltitude[x][z+1], z+1);
-					p2.reset(x, myAltitude[x][z], z);
-					p3.reset(x+1, myAltitude[x+1][z], z);
+		for (int x = 0; x < myWidth - 1; x++) {
+			for (int z = 0; z < myDepth - 1; z++) {
+				//Triangle 1 - top right, top left, bottom left,
+				p1.reset(x, myAltitude[x][z+1], z+1);
+				p2.reset(x, myAltitude[x][z], z);
+				p3.reset(x+1, myAltitude[x+1][z], z);
+				
+				normal = Helpers.calculateNormal(new Point[] {p1, p2, p3});
+				gl.glNormal3dv(normal.doubleVector(), 0);
+				
+				if (texture) {
+					// Draws the first triangle 
+					gl.glTexCoord2d(0, 0);
+					gl.glVertex3dv(p1.doubleVector(), 0);
+					gl.glTexCoord2d(1, 0);
+					gl.glVertex3dv(p2.doubleVector(), 0);
+					gl.glTexCoord2d(0, 1);
+					gl.glVertex3dv(p3.doubleVector(), 0);
+				} else {
+					// Color is calculated based on x y and z (with an aim towards higher green values).
+					Color c1 = new Color((z+1)/2, x, myAltitude[x][z+1]/2);
+					Color c2 = new Color(z/2, x, myAltitude[x][z]/2);
+					Color c3 = new Color(z/2, x+1, myAltitude[x+1][z]/2);
 					
-					normal = Helpers.calculateNormal(new Point[] {p1, p2, p3});
-					gl.glNormal3dv(normal.doubleVector(), 0);
-					
-					if (texture) {
-						// Draws the first triangle 
-							gl.glTexCoord2d(0, 0);
-						gl.glVertex3dv(p1.doubleVector(), 0);
-							gl.glTexCoord2d(1, 0);
-						gl.glVertex3dv(p2.doubleVector(), 0);
-							gl.glTexCoord2d(0, 1);
-						gl.glVertex3dv(p3.doubleVector(), 0);
-					} else {
-						// Color is calculated based on x y and z (with an aim towards higher green values).
-						Color c1 = new Color((z+1)/2, x, myAltitude[x][z+1]/2);
-						Color c2 = new Color(z/2, x, myAltitude[x][z]/2);
-						Color c3 = new Color(z/2, x+1, myAltitude[x+1][z]/2);
-						
-						// Draws the first triangle 
-							gl.glColor3dv(c1.doubleVector(), 0);
-						gl.glVertex3dv(p1.doubleVector(), 0);
-							gl.glColor3dv(c2.doubleVector(), 0);
-						gl.glVertex3dv(p2.doubleVector(), 0);
-							gl.glColor3dv(c3.doubleVector(), 0);
-						gl.glVertex3dv(p3.doubleVector(), 0);
-					}
-					//Triangle 2 -bottom left, bottom right, top right
-					p1.reset(x+1, myAltitude[x+1][z], z);
-					p2.reset(x+1, myAltitude[x+1][z+1], z+1);
-					p3.reset(x, myAltitude[x][z+1], z+1);
-					
-					normal = Helpers.calculateNormal(new Point[] {p1, p2, p3});
-					gl.glNormal3dv(normal.doubleVector(), 0);
-					 
-					if (texture) {
-						gl.glTexCoord2d(1, 0);
-						gl.glVertex3dv(p1.doubleVector(), 0);
-							gl.glTexCoord2d(1, 1);
-						gl.glVertex3dv(p2.doubleVector(), 0);
-							gl.glTexCoord2d(0, 1);
-						gl.glVertex3dv(p3.doubleVector(), 0);
-					} else {
-						// Colour is not reset here, to give the fun triangle colour effect
-						gl.glVertex3dv(p1.doubleVector(), 0);
-						gl.glVertex3dv(p2.doubleVector(), 0);
-						gl.glVertex3dv(p3.doubleVector(), 0);
-					}
-					
+					// Draws the first triangle 
+					gl.glColor3dv(c1.doubleVector(), 0);
+					gl.glVertex3dv(p1.doubleVector(), 0);
+					gl.glColor3dv(c2.doubleVector(), 0);
+					gl.glVertex3dv(p2.doubleVector(), 0);
+					gl.glColor3dv(c3.doubleVector(), 0);
+					gl.glVertex3dv(p3.doubleVector(), 0);
 				}
-	    	}
+                
+				//Triangle 2 -bottom left, bottom right, top right
+				p1.reset(x+1, myAltitude[x+1][z], z);
+				p2.reset(x+1, myAltitude[x+1][z+1], z+1);
+				p3.reset(x, myAltitude[x][z+1], z+1);
+				
+				normal = Helpers.calculateNormal(new Point[] {p1, p2, p3});
+				gl.glNormal3dv(normal.doubleVector(), 0);
+				 
+				if (texture) {
+					gl.glTexCoord2d(1, 0);
+					gl.glVertex3dv(p1.doubleVector(), 0);
+					gl.glTexCoord2d(1, 1);
+					gl.glVertex3dv(p2.doubleVector(), 0);
+					gl.glTexCoord2d(0, 1);
+					gl.glVertex3dv(p3.doubleVector(), 0);
+				} else {
+					// Colour is not reset here, to give the fun triangle colour effect
+					gl.glVertex3dv(p1.doubleVector(), 0);
+					gl.glVertex3dv(p2.doubleVector(), 0);
+					gl.glVertex3dv(p3.doubleVector(), 0);
+				}		
+			}
+    	}
+        
 	    gl.glEnd();    
 	    if (texture) {
 	    	gl.glDisable(gl.GL_TEXTURE_2D);
